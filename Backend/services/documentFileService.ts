104cef5afd0f5fb69f622290231c4199c30d1749
patrickfernandesconceicao <patrickfernandesconceicao@gmail.com>
2025-02-13 15:55:02 -0300

    pdf-parse-solved

--- Backend/services/documentFileService.ts
+++ Backend/services/documentFileService.ts
@@ -1,12 +1,27 @@
-import { Document } from "@prisma/client";
-import { User } from "../entities/user";
-import { PrismaDB } from "../prisma/client";
+import pdf from "pdf-parse";
 import { DocumentFileRepositorie } from "../repositorie/documentFileRepositorie";
 import { DocumentPdF } from "../entities/documentPdf";
 
 export class DocumentFileService {
-    
-    static getAll(data: DocumentPdF) {
-        const newDocument = DocumentFileRepositorie.create(data);
+    static async getAll(data: DocumentPdF) {
+        try {
+            if (!data.document_pdf) {
+                throw new Error("Arquivo PDF inválido ou não fornecido.");
+            }
+
+            // 🔄 Converte Uint8Array para Buffer
+            const buffer = Buffer.from(data.document_pdf);
+
+            // 🔥 Extraindo texto do PDF
+            const pdfData = await pdf(buffer);
+            const extractedText = pdfData.text;
+
+
+            console.log(extractedText)
+
+        } catch (error) {
+            console.error("Erro ao processar PDF:", error);
+            throw new Error("Falha ao processar o arquivo PDF.");
+        }
     }
 }
